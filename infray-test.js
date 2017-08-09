27f0dc0dd632e1d7a139ec611c41f6c1d21685ce
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-10 02:41:14 +0800

    Updated client/server unit test.

--- infray-test.js
+++
@@ -1,7 +0,0 @@
-
-const assert = require( "assert" );
-const infray = require( "./infray.js" );
-
-assert.deepEqual( infray( [ 1, 2, 3, 4 ], [ 3, 4, 5, 6 ] ), [ 1, 2 ], "should be equal to [ 1, 2 ]" );
-
-console.log( "ok" );
